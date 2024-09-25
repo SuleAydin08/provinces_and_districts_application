4f4e5d5b3c7da775ffc84433c37d0c2deedab6b9
saydin <saydin@tederga.com>
2024-09-25 12:24:11 +0300

    [ + ] Uygulamaya tab bar eklenmiştir. Bu tabbarlarda birinde ilçeler kısmı map içinde birisindede liste şeklinde çağrılmıştır. [ + ] Artvin_rize.json dosyası eklenmiştir.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,25 +1,34 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provinces_and_districts_application/districts.dart';
 import 'package:provinces_and_districts_application/provinces.dart';
 
-//Veriler her programlama dilinde json formatında kullanılır.
 class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  List<Province> provinces = [];
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  List<Province> provincesMap = [];
+  List<Province> provincesList = [];
+  late TabController _tabController;
 
   @override
   void initState() {
     super.initState();
-    //Aşağıda setstate fonksiyonu çağırdığımız için ekran çizilmeden setstate yapmadığımızı anlamak için burada bir kod yazarak bunu kontrol etmiş oluruz.
-    WidgetsBinding.instance.addPostFrameCallback((_) {});
-    _jsonAnalyze();
+    _tabController = TabController(length: 2, vsync: this);
+    //Burdaki amaç bu sayfayı açar açmaz bu verileri get et yani getir demek
+    _jsonAnalyzeMap();
+    _jsonAnalyzeList();
+    // _loadJsonData();
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 
   @override
@@ -28,70 +37,96 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         backgroundColor: Colors.deepOrange[100],
         title: Text("İller ve İlçeler"),
-      ),
-      body: ListView.builder(itemBuilder: _createListItem,itemCount:provinces.length),
-    );
-  }
-  //Bunu normalde en alt kısımda oluşturdu ama biz buildin hemen altına aldık.
-  Widget? _createListItem(BuildContext context, int index) {
-    //Ben eğer öğelerin içeriği tıkladığımda açılıp kapanmasını istiyorsam ExpansionTile olarak yapılır.
-    //Görüntü olarak listTiledan tek farkı öğeler arası boşluğun expansionTileda öğeler arası boşluğu daha fazla
-    return Card(
-      child: ExpansionTile(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(provinces[index].name),
-            Text(provinces[index].licensePlateCode)
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(text: 'Map Kullanımı'),
+            Tab(text: 'Liste Kullanımı'),
           ],
         ),
-        leading: Icon(Icons.location_city),
-        children: provinces[index].districts.map((district){
-          return ListTile(
-            title: Text(district.name),
-          );
-        }).toList(),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          _buildProvinceList(provincesMap),
+          _buildProvinceList(provincesList),
+        ],
       ),
     );
   }
 
-  void _jsonAnalyze() async {
-    //Json formatını String formatında okuyoruz ve biz bu okuma için rootBundle.loadString kullanıyoruz.
-    //String kısmını rootBundle.loadString ne döndürüyor ona baktı ve oan göre yazdı.
-    String jsonString =
-        await rootBundle.loadString("assets/iller_ilceler.json");
-    //Json yapısına en uygun yapı map olduğu için mapa döndürürüz.
-    Map<String, dynamic> provincesMap = json.decode(jsonString);
+  Widget _buildProvinceList(List<Province> provinces) {
+    return ListView.builder(
+      itemCount: provinces.length,
+      itemBuilder: (context, index) {
+        return Card(
+          child: ExpansionTile(
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(provinces[index].name),
+                Text(provinces[index].licensePlateCode)
+              ],
+            ),
+            leading: Icon(Icons.location_city),
+            children: provinces[index].districts.map((district) {
+              return ListTile(
+                title: Text(district.name),
+              );
+            }).toList(),
+          ),
+        );
+      },
+    );
+  }
 
-    //Her bir ilin plaka kodlarını dolanmak için
-    for (String licensePlateCode in provincesMap.keys) {
-      //il map deyip o ilin plaka kodundaki değeri
-      //Plaka kodu
-      Map<String, dynamic> provinceMap = provincesMap[licensePlateCode];
-      //İl ismi
-      String provinceName = provinceMap["name"];
-      //String mi map mi bunu jsonda verilere bakıp karar veriyoruz iller map içersinde yazıldığı için map içerisine yazarız.
-      //İlçe ismi
-      Map<String, dynamic> districtsMap = provinceMap["districts"];
+// //İki json dosyasını aynı anda yüklemek için yaptı
+//   void _loadJsonData() async {
+//     await Future.wait([_jsonAnalyzeMap(), _jsonAnalyzeList()]);
+//   }
+//json dosyasını yüklemek zaman alabilen bir işlemdir
+//bunu asenkron hale getirmek için future kullanılır.
+  Future<void> _jsonAnalyzeMap() async {
+    try {
+      String jsonString =
+          await rootBundle.loadString("assets/iller_ilceler.json");
+      Map<String, dynamic> provincesMapData = json.decode(jsonString);
 
-      //Mapin içerindeki değerleri listeye çevirme işlemi
-      List<District> allDistricts = [];
+      provincesMap = provincesMapData.entries.map((entry) {
+        String provinceName = entry.value['name'];
+        Map<String, dynamic> districtsMap = entry.value['districts'];
 
-      for (String districtCode in districtsMap.keys) {
-        Map<String, dynamic> districtMap = districtsMap[districtCode];
-        String districtName = districtMap["name"];
-        District district = District(districtName);
-        allDistricts.add(district);
-      }
+        List<District> districtObjects =
+            districtsMap.entries.map((districtEntry) {
+          return District(districtEntry.value['name']);
+        }).toList();
 
-      Province province =
-          Province(provinceName, licensePlateCode, allDistricts);
-      //Bu şekilde yaptığımızda ekranda bir çıktı alamayız.
-      // provinces.add(province);
-      //Set state burada çağırırsam for döngüsü içerinde 81 kere setstate yapmış olurum.
-      provinces.add(province);
+        return Province(provinceName, entry.key, districtObjects);
+      }).toList();
+    } catch (e) {
+      print("Error loading map usage JSON: $e");
     }
-    //Burada setstate yaparsak bütün iller eklendikten sonra bir kere setstate kullanmış oluruz.
-    setState(() {});
+  }
+
+  Future<void> _jsonAnalyzeList() async {
+    try {
+      String jsonString =
+          await rootBundle.loadString("assets/artvin_rize.json");
+      Map<String, dynamic> provincesListData = json.decode(jsonString);
+
+      provincesList = provincesListData.entries.map((entry) {
+        String provinceName = entry.value['name'];
+        List<String> districtsList =
+            List<String>.from(entry.value['districts']);
+
+        List<District> districtObjects = districtsList.map((districtName) {
+          return District(districtName);
+        }).toList();
+
+        return Province(provinceName, entry.key, districtObjects);
+      }).toList();
+    } catch (e) {
+      print("Error loading list usage JSON: $e");
+    }
   }
 }
